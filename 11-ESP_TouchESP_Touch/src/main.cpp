7b6ab44b5205e96463906664ed1d29bef53fa8fe
宁子希 <1589326497@qq.com>
2024-01-09 15:54:13 +0800

    Touch外设练习提交

---
+++ 11-ESP_TouchESP_Touch/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#define LED_Pin 2
+#define Touch_Pin 15
+
+uint16_t touchValue = 0;
+void setup()
+{
+  pinMode(LED_Pin, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  touchValue = touchRead(Touch_Pin);
+  Serial.println(touchValue);
+
+  if (touchValue < 35)
+  {
+    digitalWrite(LED_Pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_Pin, LOW);
+  }
+  
+}
